16a553286563ace1f4558a9c6ba6ee9ea2681217
Lokendra Jatav <lokendrajatav1503@gmail.com>
2025-01-11 14:29:10 +0530

    nav completed

---
+++ src/pages/About.jsx
@@ -0,0 +1,38 @@
+
+
+const About = () => {
+  return (
+
+    <section className="section-about container">
+      <h2 className="container-titlr">
+        Here are the Interesting Facts <br />
+        we&#39;re proud of
+
+      </h2>
+      <div className="gradient-cards">
+        <div className="card">
+          <p className="card-title">India</p>
+          <p>
+            <span className="card-description">Capital:</span>
+            Delhi
+          </p>
+          <p>
+            <span className="card-description">Population:</span>
+            1000
+          </p>
+          <p>
+            <span className="card-description">Interesting Fact:</span>
+            We Love India
+          </p>
+
+        </div>
+
+
+      </div>
+
+    </section>
+   
+  )
+}
+
+export default About
---
+++ src/pages/Country.jsx
@@ -0,0 +1,9 @@
+
+
+const Country = () => {
+  return (
+   <h1>Country Page</h1>
+  )
+}
+
+export default Country
