5e53c76c2f9b9c52e1dcde7781f833bbd483dfc3
Lokendra Jatav <lokendrajatav1503@gmail.com>
2025-01-15 21:59:23 +0530

    about competed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,43 +1,49 @@
-
-
+import countryFacts from "../api/countryData.json";
 const About = () => {
-  return (
+    return (
+        <section className="section-about container">
+            <h2 className="container-title">
+                Here are the Interesting Facts <br />
+                we&#39;re proud of
+            </h2>
+            <div className="gradient-cards">
+                {countryFacts.map((country) => {
+                    const {
+                        id,
+                        countryName,
+                        capital,
+                        population,
+                        interestingFact,
+                    } = country;
+                    return (
+                        <div className="card" key={id}>
+                            <div className="container-card bg-blue-box">
+                                <p className="card-title">{countryName}</p>
+                                <p>
+                                    <span className="card-description">
+                                        Capital:
+                                    </span>
+                                    {capital}
+                                </p>
+                                <p>
+                                    <span className="card-description">
+                                        Population:
+                                    </span>
+                                    {population}
+                                </p>
+                                <p>
+                                    <span className="card-description">
+                                        Interesting Fact:
+                                    </span>
+                                    {interestingFact}
+                                </p>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </section>
+    );
+};
 
-    <section className="section-about container">
-      <h2 className="container-title">
-        Here are the Interesting Facts <br />
-        we&#39;re proud of
-
-      </h2>
-      <div className="gradient-cards">
-        <div className="card">
-          <div className="container-card bg-blue-box">
-
-         
-          <p className="card-title">India</p>
-          <p>
-            <span className="card-description">Capital:</span>
-            Delhi
-          </p>
-          <p>
-            <span className="card-description">Population:</span>
-            1000
-          </p>
-          <p>
-            <span className="card-description">Interesting Fact:</span>
-            We Love India
-          </p>
-          </div>
-
-        </div>
-        
-
-
-      </div>
-
-    </section>
-   
-  )
-}
-
-export default About 
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,14 @@
-import { FaLongArrowAltRight } from "react-icons/fa";
+
+import HeroSection from "../components/UI/HeroSection";
+import About from "./About";
 
 const Home = () => {
     return (
-        <main className="hero-section main">
-            <div className="container grid grid-two-cols">
-                <div className="hero-content">
-                    <h1 className="heading-xl">
-                        Explore the World, One Country at aTime.
-                    </h1>
-                    <p className="paragraph">
-                        Discover the history, culture, and beauty of every
-                        nation. Sort, search, and filter through countries to
-                        find the details you
-                    </p>
-                    <button className="btn btn-darken btn-inline bg-white-box">
-                        Start Exploring <FaLongArrowAltRight />
-                    </button>
-                </div>
-                <div className="hero-image">
-                    <img
-                        src="/images/world.png"
-                        alt="world is beauty"
-                        className="banner-image"
-                    />
-                </div>
-            </div>
-        </main>
+        <>
+        <HeroSection></HeroSection>
+
+        <About></About>
+        </>
     );
 };
 
