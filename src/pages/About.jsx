be1a23e0a7ddd9dff1483ade3192068991881684
Lokendra Jatav <lokendrajatav1503@gmail.com>
2025-01-15 21:31:41 +0530

    cards complete

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,13 +4,16 @@ const About = () => {
   return (
 
     <section className="section-about container">
-      <h2 className="container-titlr">
+      <h2 className="container-title">
         Here are the Interesting Facts <br />
         we&#39;re proud of
 
       </h2>
       <div className="gradient-cards">
         <div className="card">
+          <div className="container-card bg-blue-box">
+
+         
           <p className="card-title">India</p>
           <p>
             <span className="card-description">Capital:</span>
@@ -24,8 +27,10 @@ const About = () => {
             <span className="card-description">Interesting Fact:</span>
             We Love India
           </p>
+          </div>
 
         </div>
+        
 
 
       </div>
@@ -35,4 +40,4 @@ const About = () => {
   )
 }
 
-export default About
+export default About 
