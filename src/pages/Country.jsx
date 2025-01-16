a201ae05b4f171c051806fdda5d283349ded399d
Lokendra Jatav <lokendrajatav1503@gmail.com>
2025-01-16 18:27:34 +0530

    filter data

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -3,10 +3,14 @@ import { useEffect } from "react";
 import Loader from "../components/UI/Loader";
 import { getCountryData } from "../api/postApi";
 import CountryCard from "../components/Layout/CountryCard";
+import SearchFilter from "../components/UI/SearchFilter";
 
 const Country = () => {
     const [isPending, startTransition] = useTransition();
     const [countries, setCountries] = useState([]);
+    const [search, setSearch] = useState();
+    const [filter, setFilter] = useState("all");
+
     useEffect(() => {
         startTransition(async () => {
             const res = await getCountryData();
@@ -15,11 +19,43 @@ const Country = () => {
     }, []);
 
     if (isPending) return <Loader />;
+
+
+
+
+
+    const searchCountry = (country) => {
+        if (search) {
+            return country.name.common
+                .toLowerCase()
+                .includes(search.toLowerCase());
+        }
+        return country;
+    };
+
+    const filterRegion = (country) => {
+        if (filter === "all") return country
+          return country.region === filter;
+    };
+
+    // here is the main logic
+
+    const filterCountries = countries.filter(
+        (country) => searchCountry(country) && filterRegion(country)
+    );
     return (
         <section className="country-section">
+            <SearchFilter
+                search={search}
+                setSearch={setSearch}
+                filter={filter}
+                setFilter={setFilter}
+                countries={countries}
+                setCountries={setCountries}
+            />
             <ul className="grid grid-four-cols">
-                {countries.map((curCountry,index) => {
-                    return <CountryCard country={curCountry} key={index}/>;
+                {filterCountries.map((curCountry, index) => {
+                    return <CountryCard country={curCountry} key={index} />;
                 })}
             </ul>
         </section>
