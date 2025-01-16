98204d0f01c4aee474b729bdf34e9a2ad04ecb0d
Lokendra Jatav <lokendrajatav1503@gmail.com>
2025-01-16 09:50:19 +0530

    footer complete

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,12 +1,79 @@
-
+import { MdPlace } from "react-icons/md";
+import footerContact from "../../api/footerApi.json";
+import { IoCallSharp } from "react-icons/io5";
+import { TbMailPlus } from "react-icons/tb";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <div>
-      <h1>Footer</h1>
-      
-    </div>
-  )
-}
+    const footerIcon = {
+        MdPlace: <MdPlace />,
+        IoCallSharp: <IoCallSharp />,
+        TbMailPlus: <TbMailPlus />,
+    };
+    return (
+        <footer className="footer-section">
+            <div className="container grid grid-three-cols">
+                {footerContact.map((curData, index) => {
+                    const { icon, title, details } = curData;
+                    return (
+                        <div className="footer-contact" key={index}>
+                            <div className="icon">{footerIcon[icon]}</div>
+                            <div className="footer-contact-text">
+                                <p>{title}</p>
+                                <p>{details}</p>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
 
-export default Footer
+            <div className="copyright-area">
+                <div className="container">
+                    <div className="grid grid-two-cols">
+                        <div className="copyright-text">
+                            <p>
+                                Copyright &copy; 2024, All Right Reserved
+                                <NavLink
+                                    to="https://thapatechnical.shop/"
+                                    target="_blank"
+                                >
+                                    LokendraJatav
+                                </NavLink>
+                            </p>
+                        </div>
+
+                        <div className="footer-menu">
+                            <ul>
+                                <li>
+                                    <NavLink to="/">Home</NavLink>
+                                </li>
+
+                                <li>
+                                    <NavLink
+                                        to="#"
+                                        target="_blank"
+                                    >
+                                        Social
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        to="https://github.com/lokendrajatav1/react-country-app"
+                                        target="_blank"
+                                    >
+                                        Source Code
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/contact">Contact</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
